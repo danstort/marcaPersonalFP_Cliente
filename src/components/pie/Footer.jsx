b24c66d8e5021a4ab9e3beae5123f14b8b3f1047
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2025-02-14 21:36:14 +0100

    hook useCounts

--- src/components/pie/Footer.jsx
+++ src/components/pie/Footer.jsx
@@ -1,61 +1,19 @@
 import '../../App.css';
 import { getCounts } from '../../services/posts/getCounts';
 import { useEffect, useState } from 'react';
+import useCounts from '../../hooks/useCounts';
+
 
 
 const Footer = () => {
 
-      // Estado con la lista de post que recuperamos de la REST API
-  const [counts, setCounts] = useState({
-    numeroEmpresas: 0,
-    numeroProyectos: 0,
-    numeroAlumnos: 0,
-  });
-
-
-  function obtenerCounts(){
-
-      
-      getCounts({tabla: "empresas"}).then(valor => {
-          
-          setCounts(counts => ({
-            ...counts,
-            numeroEmpresas: valor.count,
-          }));
-          console.log(valor);
-        });   
-        
-        getCounts({tabla: "proyectos"}).then(valor => {
-          
-          setCounts(counts => ({
-            ...counts,
-            numeroProyectos: valor.count,
-          }));
-          console.log(valor);
-        });
-
-        getCounts({tabla: "users"}).then(valor => {
-          
-          setCounts(counts => ({
-            ...counts,
-            numeroAlumnos: valor.count,
-          }));
-          console.log(valor);
-        });
-  }
-
-  
-  useEffect(obtenerCounts, []);
-
-  // Función encargada de llamar al componente Post con el post
-  // que recibe como parámetro implícito de la función map
+  const counts = useCounts();
   
     return (
         <footer className='borde row'>
             <p>Footer</p>
-            <h1>{counts.numeroEmpresas}</h1>
-            <h1>{counts.numeroProyectos}</h1>
-            <h1>{counts.numeroAlumnos}</h1>
+           {counts.buscando ? <p>Buscando...</p> :
+            <p>Hay {counts.counts.numeroEmpresas} empresas, {counts.counts.numeroProyectos} proyectos y {counts.counts.numeroAlumnos} alumnos</p>}
    
 
         </footer>
---
+++ src/hooks/useCounts.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect} from 'react';
+import { getCounts } from '../services/posts/getCounts';
+
+const useCounts = () => {
+
+const [buscando, setBuscando] = useState(false);
+
+const [counts, setCounts] = useState({
+    numeroEmpresas: 0,
+    numeroProyectos: 0,
+    numeroAlumnos: 0,
+  });
+
+
+  function obtenerCounts(){
+
+        setBuscando(true);
+      
+      getCounts({tabla: "empresas"}).then(valor => {
+          
+          setCounts(counts => ({
+            ...counts,
+            numeroEmpresas: valor.count,
+          }));
+          console.log(valor);
+        });   
+        
+        getCounts({tabla: "proyectos"}).then(valor => {
+          
+          setCounts(counts => ({
+            ...counts,
+            numeroProyectos: valor.count,
+          }));
+          console.log(valor);
+        });
+
+        getCounts({tabla: "users"}).then(valor => {
+          
+          setCounts(counts => ({
+            ...counts,
+            numeroAlumnos: valor.count,
+          }));
+          console.log(valor);
+
+          setBuscando(false);
+        });
+  }
+
+  
+  useEffect(obtenerCounts, []);
+
+
+
+    return {buscando, counts};
+
+}
+
+export default useCounts;
