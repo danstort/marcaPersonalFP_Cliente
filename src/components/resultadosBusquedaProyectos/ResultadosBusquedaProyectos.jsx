d6de2f5952e51363ac8e1c260a99a26ffa3775c0
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2025-03-01 15:35:49 +0100

    Mejoras en Busqueda Proyectos

--- src/components/resultadosBusquedaProyectos/ResultadosBusquedaProyectos.jsx
+++ src/components/resultadosBusquedaProyectos/ResultadosBusquedaProyectos.jsx
@@ -1,27 +1,44 @@
 import useProyectos from "../../hooks/useProyectos";
 import ProyectoMinCard from "../proyectoMinCard/ProyectoMinCard";
+import { useState } from "react";
 
 
 const ResultadosBusquedaProyectos = (props) => {
 
     const proyectos = useProyectos();
+    
 
     
     function mostrarProyectos(proyecto) {
         //La función some() comprueba si al menos un elemento de ciclos cumple con la condición que le pasamos.
         //La función includes() comprueba si el array de FamiliaID
-        if (proyecto.ciclos.some(ciclo => props.familiaID.includes(ciclo.familia_id))) {
+
+        if (props.familiaID.length === 0) {
             return (
                 <ProyectoMinCard key={proyecto.id} proyecto={proyecto}/>
             );
         }
-        return null;
+        else if (proyecto.ciclos.some(ciclo => props.familiaID.includes(ciclo.familia_id))) {
+            
+            return (
+                <ProyectoMinCard key={proyecto.id} proyecto={proyecto}/>
+            );
+            
+            
+        } 
+        
+        
+        
     }
 
     return (
         <div className="row ">
             
-            {proyectos.proyectos.map(mostrarProyectos)}
+            {proyectos.proyectos.map(mostrarProyectos)} 
+            {props.familiaID.length === 0 ? (
+                <p>No hay proyectos que mostrar</p>
+            ) : null}
+          
 
         </div>
     );
